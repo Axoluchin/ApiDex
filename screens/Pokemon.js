784b949f23dd17738449337b78db5145a7f3c9eb
Damian Huchin <huchin.h.damian@gmail.com>
2021-11-30 20:46:09 -0600

    Nuevas splash screen y botones en Pokemon

--- screens/Pokemon.js
+++ screens/Pokemon.js
@@ -1,38 +1,69 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+import { Icon } from "react-native-elements";
 
 const Pokemon = ({ navigation, route }) => {
-  const Poke = route.params.Poke;
+  const [Poke, setPoke] = useState(route.params.Poke);
+
+  const changePokemon = (ID) => {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + String(ID))
+      .then((value) => value.json())
+      .then((value) => {
+        setPoke(value);
+      });
+  };
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: Poke.sprites.front_default }}
-      />
-      <Text style={styles.Titulo}>
-        {Poke.name.charAt(0).toUpperCase() + Poke.name.slice(1)}
-      </Text>
-      <Text style={styles.texto}>
-          ID: {Poke.id}
-      </Text>
-      <Text style={styles.texto}>
-          Exp base: {Poke.base_experience}
-      </Text>
-      <Text style={styles.texto}>
-            Altura: {(Poke.height/10)}m
-      </Text>
-      <Text style={styles.texto}>
-          Peso: {(Poke.weight/10)}Kg.
-      </Text>
-      <Text style={styles.texto}>
-          
-      </Text>
-      <Text style={styles.texto}>
-          
-      </Text>
-      <Text style={styles.texto}>
-          
-      </Text>
+      <View style={{ flex: 1 }}>
+        <Image
+          style={styles.image}
+          source={{ uri: Poke.sprites.front_default }}
+        />
+        <Text style={styles.Titulo}>
+          {Poke.name.charAt(0).toUpperCase() + Poke.name.slice(1)}
+        </Text>
+
+        <View style={styles.statView}>
+          <Text style={styles.statTitle}>ID</Text>
+          <Text style={styles.texto}>{Poke.id}</Text>
+        </View>
+
+        <View style={styles.statView}>
+          <Text style={styles.statTitle}>Exp. al derrotarlo</Text>
+          <Text style={styles.texto}>{Poke.base_experience}</Text>
+        </View>
+
+        <View style={styles.statView}>
+          <Text style={styles.statTitle}>Altura</Text>
+          <Text style={styles.texto}>{Poke.height / 10}m</Text>
+        </View>
+
+        <View style={styles.statView}>
+          <Text style={styles.statTitle}>Peso</Text>
+          <Text style={styles.texto}>{Poke.weight / 10}Kg.</Text>
+        </View>
+        <Text style={styles.texto}></Text>
+        <Text style={styles.texto}></Text>
+        <Text style={styles.texto}></Text>
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+        <TouchableOpacity
+          style={{
+            ...styles.boton, display: (Poke.id!=1) ? "flex" : "none"
+          }}
+          onPress={() => changePokemon(Poke.id - 1)}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Anterior</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ ...styles.boton, display: (Poke.id!=898) ? "flex" : "none" }}
+          onPress={() => changePokemon(Poke.id + 1)}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>Siguiente</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -46,7 +77,7 @@ const styles = StyleSheet.create({
   },
   texto: {
     color: "white",
-    margin: 16,
+    fontSize: 20,
   },
   image: {
     height: 192,
@@ -62,8 +93,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     borderBottomWidth: 1,
-    borderColor: 'gray',
-    marginHorizontal: '10%'
+    borderColor: "gray",
+    marginHorizontal: "10%",
   },
   pokelista: {
     marginVertical: 8,
@@ -83,5 +114,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#DB2F2F",
     borderRadius: 8,
+  },
+  statView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderColor: "gray",
+  },
+  statTitle: {
+    color: "#FF5555",
+    fontWeight: "bold",
+    fontSize: 20,
   },
 });
